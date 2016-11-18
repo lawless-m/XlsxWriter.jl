006fd28a9409cabe929e4d0a3364b3548823a7ff
lawless-m <maht0x0r@xsmail.com>
2016-11-18 13:59:35 -0500

    add_format works
    no argument checking, just throws

--- XlsxWriter.jl
+++ XlsxWriter.jl
@@ -1,8 +1,9 @@
 module XlsxWriter
 
-export Workbook, add_worksheet, set_column!, write!, Url, close_workbook
+export Workbook, add_worksheet!, add_format!, set_column!, write!, Url, close_workbook
 
-typealias Format Dict{AbstractString, Bool}
+
+
 type Url
 	url::AbstractString
 end
@@ -11,12 +12,19 @@ end
 using PyCall
 @pyimport xlsxwriter
 
+
+type Format
+	fmt::Union{PyObject, Void}
+	Format() = new(nothing)
+	Format(p::PyObject) = new(p)
+end
+
 Workbook(fn::AbstractString) = xlsxwriter.Workbook(fn)
 
-add_worksheet(wb::PyObject) = wb[:add_worksheet]()
+add_worksheet!(wb::PyObject) = wb[:add_worksheet]()
 
 
-# add_format!(ws::PyObject, args...) = ws[:add_format](args...)
+add_format!(ws::PyObject, f::Dict) = Format(ws[:add_format](f))
 
 set_column!(ws::PyObject, args...) = ws[:set_column](args...)
 
@@ -32,7 +40,7 @@ function write!(ws::PyObject, row::Int64, col::Int64, data::AbstractString, fmt:
 	else
 		fn = :write_blank
 	end
-	ws[fn](row, col, data, fmt)
+	fmt.fmt == nothing ? ws[fn](row, col, data) : ws[fn](row, col, data, fmt.fmt)
 end
 function write!(ws::PyObject, cell::AbstractString, data::AbstractString, fmt::Format=Format())
 	if length(data) > 0
@@ -44,28 +52,29 @@ function write!(ws::PyObject, cell::AbstractString, data::AbstractString, fmt::F
 	else
 		fn = :write_blank
 	end
-	ws[fn](cell, data, fmt)
+	
+	fmt.fmt == nothing ? ws[fn](cell, data) : ws[fn](cell, data, fmt.fmt)
 end
 
 # write_number
-write!(ws::PyObject, row::Int64, col::Int64, num::Real, fmt::Format=Format()) = ws[:write_number](row, col, num, fmt)
-write!(ws::PyObject, cell::AbstractString, num::Real, fmt::Format=Format()) = ws[:write_number](cell, num, fmt)
+write!(ws::PyObject, row::Int64, col::Int64, num::Real, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_number](row, col, num) : ws[:write_number](row, col, num, fmt.fmt)
+write!(ws::PyObject, cell::AbstractString, num::Real, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_number](cell, num) : ws[:write_number](cell, num, fmt.fmt)
 
 #write_blank
-write!(ws::PyObject, row::Int64, col::Int64, fmt::Format=Format()) = ws[:write_blank](row, col, fmt)
-write!(ws::PyObject, cell::AbstractString, fmt::Format=Format()) = ws[:write_blank](cell, fmt)
+write!(ws::PyObject, row::Int64, col::Int64, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_blank](row, col) : ws[:write_blank](row, col, fmt.fmt)
+write!(ws::PyObject, cell::AbstractString, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_blank](cell) : ws[:write_blank](cell, fmt.fmt)
 
 # write_datetime
-write!(ws::PyObject, row::Int64, col::Int64, dt::DateTime, fmt::Format=Format()) = ws[:write_datetime](row, col, dt, fmt)
-write!(ws::PyObject, cell::AbstractString, dt::DateTime, fmt::Format=Format()) = ws[:write_datetime](cell, dt, fmt)
+write!(ws::PyObject, row::Int64, col::Int64, dt::DateTime, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_datetime](row, col, dt) : ws[:write_datetime](row, col, dt, fmt.fmt)
+write!(ws::PyObject, cell::AbstractString, dt::DateTime, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_datetime](cell, dt) : ws[:write_datetime](cell, dt, fmt.fmt)
 
 # write_boolean
-write!(ws::PyObject, row::Int64, col::Int64, bool::Bool, fmt::Format=Format()) = ws[:write_boolean](row, col, bool, fmt)
-write!(ws::PyObject, cell::AbstractString, bool::Bool, fmt::Format=Format()) = ws[:write_boolean](cell, bool, fmt)
+write!(ws::PyObject, row::Int64, col::Int64, bool::Bool, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_boolean](row, col, bool) : ws[:write_boolean](row, col, bool, fmt.fmt)
+write!(ws::PyObject, cell::AbstractString, bool::Bool, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_boolean](cell, bool) : ws[:write_boolean](cell, bool, fmt.fmt)
 
 # write_url
-write!(ws::PyObject, row::Int64, col::Int64, u::Url, fmt::Format=Format()) = ws[:write_url](row, col, u.url, fmt)
-write!(ws::PyObject, cell::AbstractString, u::Url, fmt::Format=Format()) = ws[:write_url](cell, u.url, fmt)
+write!(ws::PyObject, row::Int64, col::Int64, u::Url, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_url](row, col, u.url) : ws[:write_url](row, col, u.url, fmt.fmt)
+write!(ws::PyObject, cell::AbstractString, u::Url, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_url](cell, u.url) : ws[:write_url](cell, u.url, fmt.fmt)
 
 
 #insert_image!(ws::PyObject, args...) = ws[:insert_image](args...)
--- demo.jl
+++ demo.jl
@@ -5,19 +5,19 @@ using XlsxWriter
 
 # Create an new Excel file and add a worksheet.
 workbook = Workbook("demo.xlsx")
-worksheet = add_worksheet(workbook)
+worksheet = add_worksheet!(workbook)
 
 # Widen the first column to make the text clearer.
 set_column!(worksheet, "A:A", 20)
 
 # Add a bold format to use to highlight cells.
-#bold = add_format!(workbook, :bold=>true)
+bold = add_format!(workbook, Dict("bold"=>true))
 
 # Write some simple text.
 write!(worksheet, "A1", "Hello")
 
 # Text with formatting.
-#write!(worksheet, "A2", "World", bold)
+write!(worksheet, "A2", "World", bold)
 
 # Write some numbers, with row/column notation.
 write!(worksheet, 2, 2, 123)
