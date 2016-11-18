e3cf45e347c59af77a63959c0401ac2d04325506
lawless-m <maht0x0r@xsmail.com>
2016-11-18 14:04:03 -0500

    enabled formattting in demo

--- demo.jl
+++ demo.jl
@@ -9,9 +9,12 @@ worksheet = add_worksheet!(workbook)
 
 # Widen the first column to make the text clearer.
 set_column!(worksheet, "A:A", 20)
+set_column!(worksheet, "D:D", 21)
+set_column!(worksheet, "E:E", 22)
 
 # Add a bold format to use to highlight cells.
 bold = add_format!(workbook, Dict("bold"=>true))
+date_format = add_format!(workbook, Dict("num_format"=>"d mmmm yyyy"))
 
 # Write some simple text.
 write!(worksheet, "A1", "Hello")
@@ -23,7 +26,7 @@ write!(worksheet, "A2", "World", bold)
 write!(worksheet, 2, 2, 123)
 write!(worksheet, 3, 1, 123.456)
 write!(worksheet, 3, 2, true)
-write!(worksheet, 3, 3, now())
+write!(worksheet, 3, 3, now(), date_format)
 write!(worksheet, 3, 4, Url("http://localhost"))
 write!(worksheet, 3, 5, "=3 + 4")
 
