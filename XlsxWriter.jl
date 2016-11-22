72583208227d3299079f0ed4bd1332cbbd5322e3
lawless-m <maht0x0r@xsmail.com>
2016-11-22 15:05:39 -0500

    added formatting features

--- XlsxWriter.jl
+++ XlsxWriter.jl
@@ -1,6 +1,6 @@
 module XlsxWriter
 
-export Workbook, add_worksheet!, add_format!, set_column!, write!, Url, close_workbook, define_name!, worksheets, get_worksheet_by_name, set_first_sheet!, merge_range!, freeze_panes!, split_panes!, Xls, write_matrix!
+export workbook, add_worksheet!, add_format!, set_column!, write!, Url, close_workbook, define_name!, worksheets, get_worksheet_by_name, set_first_sheet!, merge_range!, freeze_panes!, split_panes!, Xls, write_matrix!, set_font_name!, set_font_size!, set_font_color!, set_bold!, set_italic!, set_underline!, set_font_strikeout!, set_font_script!, set_num_format!, set_locked!, set_hidden!, set_align!, set_center_across!, set_text_wrap!, set_rotation!, set_indent!, set_shrink!, set_text_justlast!, set_pattern!, set_bg_color!, set_fg_color!, set_border!, set_bottom!, set_top!, set_left!, set_right!, set_border_color!, set_bottom_color!, set_top_color!, set_left_color!, set_right_color!, set_diag_border!, set_diag_type!, set_diag_color!
 
 type Url
 	url::AbstractString
@@ -9,11 +9,20 @@ end
 using PyCall
 @pyimport xlsxwriter
 
-type Format
-	fmt::Union{PyObject, Void}
-	Format() = new(nothing)
-	Format(p::PyObject) = new(p)
+
+type Workbook
+	py::PyObject
 end
+
+type Worksheet
+	py::PyObject
+end
+
+type Format
+	py::PyObject
+end
+
+typealias MaybeFormat Union{Format, Void}
 
 function rc2cell(row::Int, col::Int64)
 	cell = string(Char(mod(col, 26) + 65)) * "$(row+1)"
@@ -25,20 +34,18 @@ function rc2cell(row::Int, col::Int64)
 	cell
 end
 
-Workbook(fn::AbstractString) = xlsxwriter.Workbook(fn)
+workbook(fn::AbstractString) = Workbook(xlsxwriter.Workbook(fn))
 
-add_worksheet!(wb::PyObject) = wb[:add_worksheet]()
-add_worksheet!(wb::PyObject, name::AbstractString) = wb[:add_worksheet](name)
+add_worksheet!(wb::Workbook) = Worksheet(wb.py[:add_worksheet]())
+add_worksheet!(wb::Workbook, name::AbstractString) = Worksheet(wb.py[:add_worksheet](name))
 
+add_format!(wb::Workbook, f::Dict) = Format(wb.py[:add_format](f))
 
-
-add_format!(ws::PyObject, f::Dict) = Format(ws[:add_format](f))
-
-set_column!(ws::PyObject, args...) = ws[:set_column](args...)
+set_column!(ws::Worksheet, args...) = ws.py[:set_column](args...)
 
 # write_string / write_formula
 
-function write!(ws::PyObject, cell::AbstractString, data::AbstractString, fmt::Format=Format())
+function write!(ws::Worksheet, cell::AbstractString, data::AbstractString, fmt::MaybeFormat=nothing)
 	if length(data) > 0
 		if data[1] == '='
 			fn = :write_formula
@@ -48,60 +55,97 @@ function write!(ws::PyObject, cell::AbstractString, data::AbstractString, fmt::F
 	else
 		fn = :write_blank
 	end
-	
-	fmt.fmt == nothing ? ws[fn](cell, data) : ws[fn](cell, data, fmt.fmt)
+	write!(ws, cell, fn, data, fmt)
+end
+
+function write!(ws::Worksheet, cell::AbstractString, fn::Symbol, data, fmt::Void)
+	ws.py[fn](cell, data)
+end
+
+function write!(ws::Worksheet, cell::AbstractString, fn::Symbol, data, fmt::Format)
+	ws.py[fn](cell, data, fmt.py)
 end
 
 # convert r,c into cell format
-write!(ws::PyObject, row::Int64, col::Int64, data, fmt::Format=Format()) = write!(ws, rc2cell(row, col), data, fmt)
+write!(ws::Worksheet, row::Int64, col::Int64, data, fmt::MaybeFormat=nothing) = write!(ws, rc2cell(row, col), data, fmt)
+write!(ws::Worksheet, cell::AbstractString, num::Real, fmt::MaybeFormat=nothing) = write!(ws, cell, :write_number, num, fmt)
+write!(ws::Worksheet, cell::AbstractString, fmt::MaybeFormat=nothing) = write!(ws, cell, :write_blank, fmt)
+write!(ws::Worksheet, cell::AbstractString, dt::DateTime, fmt::MaybeFormat=nothing) = write!(ws, cell, :write_datetime, dt, fmt)
+write!(ws::Worksheet, cell::AbstractString, bool::Bool, fmt::MaybeFormat=nothing) = write!(ws, cell, :write_boolean, bool, fmt)
+write!(ws::Worksheet, cell::AbstractString, u::Url, fmt::MaybeFormat=nothing) = write!(ws, cell, :write_url, u.url, fmt)
 
-# write_number
-write!(ws::PyObject, cell::AbstractString, num::Real, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_number](cell, num) : ws[:write_number](cell, num, fmt.fmt)
-
-#write_blank
-write!(ws::PyObject, cell::AbstractString, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_blank](cell) : ws[:write_blank](cell, fmt.fmt)
-
-# write_datetime
-write!(ws::PyObject, cell::AbstractString, dt::DateTime, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_datetime](cell, dt) : ws[:write_datetime](cell, dt, fmt.fmt)
-
-# write_boolean
-write!(ws::PyObject, cell::AbstractString, bool::Bool, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_boolean](cell, bool) : ws[:write_boolean](cell, bool, fmt.fmt)
-
-# write_url
-write!(ws::PyObject, cell::AbstractString, u::Url, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:write_url](cell, u.url) : ws[:write_url](cell, u.url, fmt.fmt)
-
-#write matrix
-
-function write_matrix!(ws::PyObject, row::Int64, col::Int64, data::Array, fmt::Format=Format())
+function write!(ws::Worksheet, row::Int64, col::Int64, data::Array, fmt::MaybeFormat=nothing)
 	re = size(data, 1)-1
 	ce = size(data, 2)-1
 	for r in 0:re, c in 0:ce
-		write!(ws, rc2cell(row+r, col+c), data[r+1, c+1], fmt)
+		write!(ws, row+r, col+c, data[r+1, c+1], fmt)
 	end
 end
 
-define_name!(wb::PyObject, name::AbstractString, target::AbstractString) = wb[:define_name](name, target)
+define_name!(wb::Workbook, name::AbstractString, target::AbstractString) = wb.py[:define_name](name, target)
 
-worksheets(wb::PyObject) = wb[:worksheets]()
+worksheets(wb::Workbook) = wb.py[:worksheets]()
 
-get_worksheet_by_name(wb::PyObject) = wb[:get_worksheet_by_name]()
+get_worksheet_by_name(wb::Workbook) = wb.py[:get_worksheet_by_name]()
 
-set_first_sheet!(ws::PyObject) = ws[:set_first_sheet]()
+set_first_sheet!(ws::Worksheet) = ws.py[:set_first_sheet]()
 
-merge_range!(ws::PyObject, first_row::Int64, first_col::Int64, last_row::Int64, last_col::Int64, contents, fmt::Format=Format()) = fmt.fmt==nothing ? ws[:merge_range](first_row, first_col, last_row, last_col, contents) : ws[:merge_range](first_row, first_col, last_row, last_col, contents, fmt.fmt) 
+merge_range!(ws::Worksheet, first_row::Int64, first_col::Int64, last_row::Int64, last_col::Int64, contents, fmt::MaybeFormat=nothing) = merge_range!(first_row, first_col, last_row, last_col, contents, fmt)
+
+merge_range!(ws::Worksheet, first_row::Int64, first_col::Int64, last_row::Int64, last_col::Int64, contents, fmt::Format) = ws.py[:merge_range](first_row, first_col, last_row, last_col, contents, fmt.py)
+
+merge_range!(ws::Worksheet, first_row::Int64, first_col::Int64, last_row::Int64, last_col::Int64, contents, fmt::Void) = ws.py[:merge_range](first_row, first_col, last_row, last_col, contents)
 
 
-freeze_panes!(ws::PyObject, row::Int64, col::Int64) = ws[:freeze_panes](row, col)
-freeze_panes!(ws::PyObject, cell::AbstractString) = ws[:freeze_panes](cell)
-freeze_panes!(ws::PyObject, row::Int64, col::Int64, top_row::Int64) = ws[:freeze_panes](row, col, top_row)
-freeze_panes!(ws::PyObject, row::Int64, col::Int64, top_row::Int64, left_col::Int64) = ws[:freeze_panes](row, col, top_row, left_col)
+freeze_panes!(ws::Worksheet, row::Int64, col::Int64) = ws.py[:freeze_panes](row, col)
+freeze_panes!(ws::Worksheet, cell::AbstractString) = ws.py[:freeze_panes](cell)
+freeze_panes!(ws::Worksheet, row::Int64, col::Int64, top_row::Int64) = ws.py[:freeze_panes](row, col, top_row)
+freeze_panes!(ws::Worksheet, row::Int64, col::Int64, top_row::Int64, left_col::Int64) = ws.py[:freeze_panes](row, col, top_row, left_col)
 
-split_panes!(ws::PyObject,x::Int64, y::Int64) = ws[:split_panes](x, y)
-split_panes!(ws::PyObject,x::Int64, y::Int64, top_row::Int64) = ws[:split_panes](x, y, top_row)
-split_panes!(ws::PyObject,x::Int64, y::Int64, top_row::Int64, left_col::Int64) = ws[:split_panes](x, y, top_row, left_col)
+split_panes!(ws::Worksheet,x::Int64, y::Int64) = ws.py[:split_panes](x, y)
+split_panes!(ws::Worksheet,x::Int64, y::Int64, top_row::Int64) = ws.py[:split_panes](x, y, top_row)
+split_panes!(ws::Worksheet,x::Int64, y::Int64, top_row::Int64, left_col::Int64) = ws.py[:split_panes](x, y, top_row, left_col)
+
+set_font_name!(fmt::Format, name::AbstractString) = fmt.py[:set_font_name](name)
+set_font_size!(fmt::Format, sz::Int64) = fmt.py[:set_font_size](sz)
+set_font_color!(fmt::Format, color::AbstractString) = fmt.py[:set_font_color](color)
+set_bold!(fmt::Format, state::Bool=true) = fmt.py[:set_bold](state)
+set_italic!(fmt::Format, state::Bool=true) = fmt.py[:set_italic](state)
+set_underline!(fmt::Format, state::Bool=true) = fmt.py[:set_underline](state)
+set_font_strikeout!(fmt::Format, state::Bool=true) = fmt.py[:set_font_strikeout](state)
+set_font_script!(fmt::Format, state::Int64) = fmt.py[:set_font_script](state)
+set_num_format!(fmt::Format, format::AbstractString) = fmt.py[:set_font_num_format](format)
+set_locked!(fmt::Format, state::Bool=true) = fmt.py[:set_locked](state)
+set_hidden!(fmt::Format, state::Bool=true) = fmt.py[:set_hidden](state)
+set_align!(fmt::Format, alignment::AbstractString) = fmt.py[:set_align](alignment)
+set_center_across!(fmt::Format, across::AbstractString) = fmt.py[:set_center_across](across)
+set_text_wrap!(fmt::Format, state::Bool=true) = fmt.py[:set_text_wrap](state)
+set_rotation!(fmt::Format, angle::Int64) = fmt.py[:set_rotation](angle)
+set_indent!(fmt::Format, level::Int64) = fmt.py[:set_indent](level)
+set_shrink!(fmt::Format, state::Bool=true) = fmt.py[:set_shrink](state)
+set_text_justlast!(fmt::Format, state::Bool=true) = fmt.py[:set_text_justlast](state)
+set_pattern!(fmt::Format, index::Int64) = fmt.py[:set_pattern](index)
+set_bg_color!(fmt::Format, color::AbstractString) = fmt.py[:set_bg_color](color)
+set_fg_color!(fmt::Format, color::AbstractString) = fmt.py[:set_fg_color](color)
+set_border!(fmt::Format, style::Int64) = fmt.py[:set_border](style)
+set_bottom!(fmt::Format, style::Int64) = fmt.py[:set_bottom](style)
+set_top!(fmt::Format, style::Int64) = fmt.py[:set_top](style)
+set_left!(fmt::Format, style::Int64) = fmt.py[:set_left](style)
+set_right!(fmt::Format, style::Int64) = fmt.py[:set_right](style)
+set_border_color!(fmt::Format, color::AbstractString) = fmt.py[:set_border_color](color)
+set_bottom_color!(fmt::Format, color::AbstractString) = fmt.py[:set_bottom_color](color)
+set_top_color!(fmt::Format, color::AbstractString) = fmt.py[:set_top_color](color)
+set_left_color!(fmt::Format, color::AbstractString) = fmt.py[:set_left_color](color)
+set_right_color!(fmt::Format, color::AbstractString) = fmt.py[:set_right_color](color)
+set_diag_border!(fmt::Format, style::Int64) = fmt.py[:set_diag_border](style)
+set_diag_type!(fmt::Format, style::Int64) = fmt.py[:set_diag_type](style)
+set_diag_color!(fmt::Format, color::AbstractString) = fmt.py[:set_diag_color](style)
+
+
+
 
 #insert_image!(ws::PyObject, args...) = ws[:insert_image](args...)
 
-close_workbook(wb::PyObject) = wb[:close]()
+close_workbook(wb::Workbook) = wb.py[:close]()
 
 end
--- demo.jl
+++ demo.jl
@@ -4,31 +4,38 @@ using XlsxWriter
 
 
 # Create an new Excel file and add a worksheet.
-workbook = Workbook("demo.xlsx")
-worksheet = add_worksheet!(workbook)
+wb = workbook("demo.xlsx")
+ws = add_worksheet!(wb)
 
 # Widen the first column to make the text clearer.
-set_column!(worksheet, "A:A", 20)
-set_column!(worksheet, "D:D", 21)
-set_column!(worksheet, "E:E", 22)
+set_column!(ws, "A:A", 20)
+set_column!(ws, "D:D", 21)
+set_column!(ws, "E:E", 22)
 
 # Add a bold format to use to highlight cells.
-bold = add_format!(workbook, Dict("bold"=>true))
-date_format = add_format!(workbook, Dict("num_format"=>"d mmmm yyyy"))
+bold = add_format!(wb, Dict("bold"=>true))
+set_font_name!(bold, "Courier New")
+set_font_size!(bold, 16)
 
+date_format = add_format!(wb, Dict("num_format"=>"d mmmm yyyy"))
+set_font_color!(date_format, "red")
 # Write some simple text.
-write!(worksheet, "A1", "Hello")
+write!(ws, "A1", "Hello")
 
 # Text with formatting.
-write!(worksheet, 1,1, "World", bold)
+write!(ws, 1,1, "World", bold)
 
 # Write some numbers, with row/column notation.
-write!(worksheet, 2, 2, 123)
-write!(worksheet, 3, 1, 123.456)
-write!(worksheet, 3, 2, true)
-write!(worksheet, 3, 3, now(), date_format)
-write!(worksheet, 3, 4, Url("http://localhost"))
-write!(worksheet, 3, 5, "=3 + 4")
+write!(ws, 2, 2, 123)
+write!(ws, 3, 1, 123.456)
+write!(ws, 3, 2, true)
+write!(ws, 3, 3, now(), date_format)
+write!(ws, 3, 4, Url("http://localhost"))
+write!(ws, 3, 5, "=3 + 4")
+write!(ws, 3, 6, [["6" 7 8.8]; ["4x6" 47 4.7]])
 
+freeze_panes!(ws, 1, 1)
 
-close_workbook(workbook)
+merge_range!(ws, 4, 9, 4, 12, "Merged")
+
+close_workbook(wb)
